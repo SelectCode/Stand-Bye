96c760e6c527d5c46b53028960956fbf6afa970e
vavido <matz.weirich@gmail.com>
2016-02-07 13:56:40 +0100

    switch to chartist, postpone implementation of chart

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,17 +1,8 @@
-var data = [
-    {
-        value: 300,
-        color: "#F7464A",
-        highlight: "#FF5A5E",
-        label: "Nicht Genervt"
-    },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Genervt von Windows-Standby"
-    }
-]
+var data = {
+    labels: ["Genervt", "Nicht genervt"],
+    series: [
+        [20, 24]
+    ]
+};
 
-var ctx = document.getElementById("chart_request").getContext("2d");
-var chart = new Chart(ctx).Pie(data);
+new Chartist.Bar('.chart_request', data);
