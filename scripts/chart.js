b0cc72494dbd7397d66fac846d9edc67f09c52f2
vavido <matz.weirich@gmail.com>
2016-03-06 12:13:54 +0100

    added more diagramms to statistics section

--- scripts/chart.js
+++ scripts/chart.js
@@ -7,54 +7,68 @@ function em(input) {
     return (emSize * input);
 }
 
-var chartData = {
-    labels: ["Von Windows-Standby genervt", "Nicht genervt"],
-    series: [
+var surveyChartData = {
+        labels: ["Von Windows-Standby genervt", "Nicht genervt"],
+        series: [
         //nervt, !nervt
-        [5, 8], // !aktiviert
-        [15, 16] //aktiviert
+            [5, 8], // !aktiviert
+            [15, 16] //aktiviert
     ],
-    seriesLabels: ["Standby aktiviert", "Standby deaktiviert"]
-};
-
-var options = {
-    stackBars: true,
-    axisX: {
-        showGrid: false
+        seriesLabels: ["Standby aktiviert", "Standby deaktiviert"],
+        unit: ""
     },
-    axisY: {
-        label: "Benutzer"
-    }
-};
+    example2Data = {
+        labels: ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"],
+        series: [
+            [15, 17, 18, 13, 17],
+            [8, 11, 9, 4, 7]
+        ],
+        seriesLabels: ["Mit Stand-Bye ", "Ohne Stand-Bye "],
+        unit: "kWh"
+    },
+    example1Data = {
+        labels: ["Ohne Stand-Bye!", "Mit Stand-Bye!"],
+        series: [
+            [100, 0],
+            [0, 75]
+        ],
+        seriesLabels: ["Mit Stand-Bye", "Ohne Stand-Bye"],
+        unit: "kWh"
+    },
+    surveyChartOptions = {
+        stackBars: true,
+        axisX: {
+            showGrid: false
+        },
+        axisY: {
+            label: "Benutzer"
+        }
+    },
+    exampleOptions = {
+        stackBars: false,
+        axisX: {
+            showGrid: false
+        },
+        axisY: {
+            label: "kWh"
+        }
+    };
 
-$(function () {
+function tooltip(chart, $wrapper) {
 
     "use strict";
 
-    $(".chart").html("");
-
-    var chart = new Chartist.Bar('.chart', chartData, options),
-        $tooltip = $("#tooltip"),
-        $tooltip_color = $("#tooltip span.color"),
-        $tooltip_text = $("#tooltip span.text"),
-        $wrapper = $('#chart_wrapper'),
-        $chart = $(".chart");
-
-    /*  $("#chart_wrapper").mousemove(function (e) {
-
-          $tooltip.css({
-              top: e.pageY + em(0.7),
-              left: e.pageX + em(0.7)
-          });
-
-      });*/
+    var $tooltip = $wrapper.find('#tooltip'),
+        $tooltip_color = $tooltip.find("span.color"),
+        $tooltip_text = $tooltip.find("span.text"),
+        $chart = $wrapper.find('.chart');
 
     $wrapper.on("mouseenter", ".ct-bar", function () {
 
         if ($(this).parents(".ct-series-a").length === 0) {
-            $tooltip_text.text(chartData.seriesLabels[0]);
+            $tooltip_text.text(chart.data.seriesLabels[0]);
         } else {
-            $tooltip_text.text(chartData.seriesLabels[1]);
+            $tooltip_text.text(chart.data.seriesLabels[1]);
         }
 
         var x1 = parseFloat($(this).attr("x1")),
@@ -69,7 +83,7 @@ $(function () {
             left: left + (x1 + (stroke / 2))
         });
 
-        $tooltip_text.append(": " + $(this).attr("ct:value"));
+        $tooltip_text.append(": " + $(this).attr("ct:value") + " " + chart.data.unit);
         $tooltip_color.css({
             background: $(this).css("stroke")
         });
@@ -79,5 +93,21 @@ $(function () {
         $tooltip.hide(0);
     });
 
+
+}
+
+$(function () {
+
+    "use strict";
+
+    $(".chart").html("");
+
+    var surveyChart = new Chartist.Bar('.chart#survey', surveyChartData, surveyChartOptions),
+        example1Chart = new Chartist.Bar('.chart#example1', example1Data, exampleOptions),
+        example2Chart = new Chartist.Bar('.chart#example2', example2Data, exampleOptions);
+
+    tooltip(surveyChart, $('.chart#survey').parent());
+    tooltip(example1Chart, $('.chart#example1').parent());
+    tooltip(example2Chart, $('.chart#example2').parent());
 
 });
