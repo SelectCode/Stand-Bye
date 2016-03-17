4611e65cea2a272a67b1767032b26250e682d74e
Florian Baader <flobaader@web.de>
2016-03-17 17:23:00 +0100

    fixed not needed include

--- SourceCode/StandBye/StandBye/ProcessSelectionForm.h
+++ SourceCode/StandBye/StandBye/ProcessSelectionForm.h
@@ -19,7 +19,6 @@ namespace StandBye {
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
 	using namespace System::Windows::Forms;
-	using namespace System::Data;
 	using namespace System::Drawing;
 
 	/// <summary>
