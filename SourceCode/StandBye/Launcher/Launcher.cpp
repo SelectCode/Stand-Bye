a25c608d264a62790ffd068c67a633e9792daaef
vavido <matz.weirich@gmail.com>
2016-08-03 23:55:13 +0200

    createprocess working now

--- SourceCode/StandBye/Launcher/Launcher.cpp
+++ SourceCode/StandBye/Launcher/Launcher.cpp
@@ -4,10 +4,10 @@
 #include "stdafx.h"
 #include "Launcher.h"
 #include <string>
-#include <iostream>
+#include <vector>
 
-#define COMMAND _T("PORTABLE")
-#define RELPATH "\\bin\\StandBye.exe"
+#define RELPATH std::wstring(L"\\bin\\StandBye.exe")
+#define LOG_D(x) OutputDebugString(x)
 
 LPCWSTR toL(std::string s) {
 	std::wstring stemp = std::wstring(s.begin(), s.end());
@@ -15,41 +15,57 @@ LPCWSTR toL(std::string s) {
 	return sw;
 }
 
-std::string getExePath() {
+std::wstring getExePath() {
 	wchar_t result[MAX_PATH];
 	GetModuleFileName(NULL, result, MAX_PATH);
-	std::wstring ws(result);
+	std::wstring moduleFileName(result);
 
-	std::string moduleFileName(ws.begin(), ws.end() );
+	std::wstring baseDir = moduleFileName.substr(0, moduleFileName.find_last_of(L"\\/"));
 
-	std::string baseDir = moduleFileName.substr(0, moduleFileName.find_last_of("\\/"));
-
-	std::string exePath = baseDir + RELPATH;
+	std::wstring exePath = baseDir + RELPATH;
 
 	return exePath;
 
 }
 
-std::string getCommand() {}
-
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR    lpCmdLine, _In_ int nCmdShow) {
 
-	OutputDebugString(toL(getExePath() + "\n"));
+    //Get exe path
+	std::wstring string = getExePath();
 
-	/**
+	//Convert to LPWSTR
+	std::vector<wchar_t> vec(string.begin(), string.end());
+	vec.push_back(L'\0');
+	LPWSTR path = &vec[0];
+
+	LOG_D(path);
+	LOG_D(L"\n");
+
 	PROCESS_INFORMATION ProcessInfo; //
-	STARTUPINFO StartupInfo; //Unused, but necessary for createprocess
-
-	ZeroMemory(&StartupInfo, sizeof(StartupInfo));
-	StartupInfo.cb = sizeof StartupInfo; //Only compulsory field
+	STARTUPINFO si; //Unused, but necessary for createprocess
 
 
-	if(CreateProcess(getExePaht(),COMMAND,
-		NULL, NULL, FALSE, 0, NULL,
-		NULL, &StartupInfo, &ProcessInfo)) {
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof si; //Only compulsory field
+
+	wchar_t command[] = L"-PORTABLE"; //Must be editable, cannot be literal
+
+	if(CreateProcessW(path, //path to executable
+		command, //cmd params
+		NULL,
+		NULL,
+		FALSE,
+		0,
+		NULL,
+		NULL,
+		&si,// in param on how to create the process
+		&ProcessInfo // out param, info on created process
+		)) {
+		LOG_D(_T("process created\n"));
 	} else {
-		
-	}*/
+		LOG_D(_T("process not created\n"));
+		//LOG_D((LPCWSTR(GetLastError())));
+	}
 
 	return 0;
 }
