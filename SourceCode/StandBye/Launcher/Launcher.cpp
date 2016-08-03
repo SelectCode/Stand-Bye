c1efd48d978bc9f98829704fc755107be570c456
vavido <matz.weirich@gmail.com>
2016-08-03 17:28:08 +0200

    create launcher Project

---
+++ SourceCode/StandBye/Launcher/Launcher.cpp
@@ -0,0 +1,16 @@
+// Launcher.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "Launcher.h"
+
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow) {
+
+
+	return 0;
+}
